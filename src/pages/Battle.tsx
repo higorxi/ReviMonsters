df26804d898abc8305de654140cf7e5b4fb86c1a
higorxi <higorgiovane7@gmail.com>
2024-12-09 16:20:36 -0300

    fix-style/corrigindo alteração no arquivo vite.config.ts

--- src/pages/Battle.tsx
+++ src/pages/Battle.tsx
@@ -1,9 +1,7 @@
-export default function Battle () {
-    return(
-        <>
-        <p>
-            Página de Batalha
-        </p>
-        </>
-    )
+export default function Battle() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center w-full">
+      <h1 className="text-4xl font-bold text-red-500">Bem-vindo a Batalha!</h1>
+    </div>
+  );
 }
